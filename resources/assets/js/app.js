12e8567ea70ca3b5bab153ce955c3f28cabf7239
Adrian Boisclair <adrianboisclair@gmail.com>
2017-09-04 00:18:09 -0700

    creates nav component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,7 +1,7 @@
 import './bootstrap';
-//require('./bootstrap'); // Import Default Laravel JS
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Nav from './components/Nav';
 
 const appContainer = document.getElementById('app-container');
 
@@ -9,12 +9,9 @@ class App extends Component
 {
   render() {
     return (
-      <div class="col-md-8 col-md-offset-2">
-        <div class="panel panel-default">
-          <div class="panel-heading">Dashboard</div>
-          <div class="panel-body">You are logged in!</div>
+      <div>
+        <Nav/>
       </div>
-    </div>
     );
   }
 }
